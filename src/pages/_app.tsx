4c9db8b5f7bfcddd4bfd53ecd8abea4b92277523
ThomasLeveque <thomasl5895@gmail.com>
2020-07-20 16:10:33 +0200

    Remove _document and fix styled-component error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import { ThemeProvider } from "styled-components";
 import { myTheme } from "../theme/my-theme";
@@ -8,10 +9,19 @@ import { GlobalStyles } from "@styles/global.styles";
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider theme={myTheme}>
-      <GlobalStyles />
-      <Component {...pageProps} key={router.route} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={myTheme}>
+        <GlobalStyles />
+        <Component {...pageProps} key={router.route} />
+      </ThemeProvider>
+    </>
   );
 };
 
