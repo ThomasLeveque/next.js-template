df75e80fbebf37f23c08779489cdc86b9aa98f50
Thomas Leveque <thomasl5895@gmail.com>
2020-09-04 15:10:33 +0200

    Add styled-reset to globalStyle

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  
   html,
   body {
     font-family: 'Montserrat', sans-serif;
